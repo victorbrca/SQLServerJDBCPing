0a7c5d4b0e8956408532454362b88ab37b33a902
Victor Mendonca <victorbrca@users.noreply.github.com>
2021-05-25 17:53:59 -0400

    Update SQLServerJDBCPing.java

--- SQLServerJDBCPing.java
+++ SQLServerJDBCPing.java
@@ -10,7 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class ConnectURL {
+public class SQLServerJDBCPing {
     public static void main(String[] args) {
 
         //Checks args
